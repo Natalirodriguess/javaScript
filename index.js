8a8e03203072766948683db235ea83765460022c
Natalirodriguess <natalyrodrigues43210@gmail.com>
2025-10-05 20:28:48 -0300

    conceito de const

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log('Hello world!');
+
+let primeiroNome = 'Natalí';
+let sobrenome = 'Rodrigues da Silva Chaves';
+
+let idade = 25;
+console.log(primeiroNome + ' ' + sobrenome+ ' de idade: ' + idade)
+// Conceito de constante (valor fixo)
+const valorFixo = 30;
+valorFixo = 20;
+ 
