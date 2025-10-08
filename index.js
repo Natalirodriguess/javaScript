17a42cb7107f6b1ea25e1bcb400e98360e2011f2
natali-rodrigues <natali.silva@rpe.tech>
2025-10-07 21:39:05 -0300

    operadores de atribuição e comparação em javaScript

--- index.js
+++ index.js
@@ -7,5 +7,14 @@ let idade = 25;
 console.log(primeiroNome + ' ' + sobrenome+ ' de idade: ' + idade)
 // Conceito de constante (valor fixo)
 const valorFixo = 30;
-valorFixo = 20;
+//valorFixo = 20;
+
+ // Operadores de atribuição
+ let numeroUm = 20
  
+ console.log(numeroUm)
+
+ //Comparação
+ console.log(numeroUm>22)
+ console.log(numeroUm === 20) //igualdade
+ console.log(numeroUm!== 20) //desigualdade
