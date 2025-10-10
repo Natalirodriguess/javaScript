b46bf05b235f0855c03085bf9f156f1b0bc57356
Natalirodriguess <natalyrodrigues43210@gmail.com>
2025-10-09 23:33:04 -0300

    factory and blueprint

--- OPP/index.js
+++ OPP/index.js
@@ -14,3 +14,25 @@ let livro = {
 livro.imprimirLivro()
 console.log(livro.autor)
 
+// criando uma fatory
+function createBook (title, author, pages){ //factory
+
+    const book = { // criando este objeto 'book'
+        title: title,
+        author: author,
+        pages: pages,
+        printBook : function(){
+            console.log('Printing...')
+        }
+    }
+
+    return book
+}
+const book1 = createBook('livro1', 'autor1', 306)
+const book2 = createBook('livro2', 'autor2', 406)
+
+console.log(book1)
+console.log(book2)
+
+
+// criando propriedade exclusiva de um objeto
