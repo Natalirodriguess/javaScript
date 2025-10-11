c7a3f885a63928ec8869efe13e5559693d25a5c3
natali-rodrigues <natali.silva@rpe.tech>
2025-10-10 23:51:33 -0300

    trabalhando com strings

---
+++ Outros/index.js
@@ -0,0 +1,14 @@
+//trabalhando com strings
+let mensagem = 'Hey, my name is Natali'
+console.log(mensagem.startsWith('hey'))
+
+console.log(mensagem.includes('Natali')) //existe substring com 'Natali'?
+
+console.log(mensagem.endsWith('tali')) // string termina com 'tali'?
+
+console.log(mensagem.at(5)) //achar caractere na posição indicada
+console.log(mensagem.charAt(2))
+console.log(mensagem.concat(' Rodrigues'))
+
+
+// 
